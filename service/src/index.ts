7e130bf12cf6eadc15d1420a2284142e8946a3d8
fungoindia <83899086+fungoindia@users.noreply.github.com>
2023-03-09 13:32:34 +0800

    Update index.ts

--- service/src/index.ts
+++ service/src/index.ts
@@ -21,10 +21,30 @@ router.post('/chat-process', async (req, res) => {
   try {
     const { prompt, options = {} } = req.body as { prompt: string; options?: ChatContext }
     let firstChunk = true
-    await chatReplyProcess(prompt, options, (chat: ChatMessage) => {
-      res.write(firstChunk ? JSON.stringify(chat) : `\n${JSON.stringify(chat)}`)
-      firstChunk = false
-    })
+    let ifAPI = false
+    let userAPI = ''
+    const words: string[] = prompt.split('!@#:')
+    userAPI = JSON.parse(words[1]).data.userInfo.description
+    if (!words.includes('sk'))
+      ifAPI = false
+    if (!ifAPI) {
+      // .concat('---只回答---符号前面的内容   句尾换两行加上以下内容： 欢迎使用GPT，您未填写APIKEY，最多输出300字，请点击这里购买：https://tdchat.com/   句尾这句话必须加上并且不算字数 最多回答我不超过300字')
+      await chatReplyProcess(words[0], options, (chat: ChatMessage) => {
+        res.write(firstChunk ? JSON.stringify(chat) : `\n${JSON.stringify(chat)}`)
+        firstChunk = false
+        // const reply = `${JSON.stringify(chat)}`
+        // const truncatedReply = reply.length > 200 ? `${reply.slice(0, 200)}...` : reply
+        // res.write(firstChunk ? JSON.stringify({ ...chat, message: truncatedReply }) : `\n${JSON.stringify({ ...chat, message: truncatedReply })}\n\nThis is an automated response.`)
+        // firstChunk = false
+      })
+    }
+    else {
+      await chatReplyProcess(words[0], options, (chat: ChatMessage) => {
+        res.write(firstChunk ? JSON.stringify(chat) : `\n${JSON.stringify(chat)}`)
+        firstChunk = false
+      })
+    }
+    globalThis.console.log(prompt, userAPI, ifAPI)
   }
   catch (error) {
     res.write(JSON.stringify(error))
